ed7739690d52c4006019b9c5530397a3902798d7
Adrian-Buczek <buczekadrian88@gmail.com>
2026-06-08 21:50:55 +0200

    Add files via upload

---
+++ egzaminwyprawy/index.php
@@ -0,0 +1,23 @@
+<?php
+//SELECT nazwa FROM miejsca ORDER BY nazwa ASC;
+//SELECT cena FROM miejsca WHERE nazwa = "Malbork"
+//SELECT nazwa, cena, link_obraz FROM miejsca WHERE link_obraz LIKE "0%";
+//SELECT m.nazwa, ROUND(AVG(w.liczba_doroslych), 2) FROM miejsca m INNER JOIN wycieczki w ON w.id_miejsca = m.id_miejsca GROUP BY m.nazwa;
+//CREATE TABLE klienci (
+    //id_klienta INT UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    //imie VARCHAR(20),
+    //nazwisko VARCHAR(40),
+    //data_ur DATE
+//);
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
